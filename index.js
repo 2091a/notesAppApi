cd99e0ab4608a22c16e7ca14faab2dff4007db75
2091a <anshulbhattglt@gmail.com>
2025-11-10 13:32:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('tweeter anshul')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

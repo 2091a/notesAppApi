6ff9d418796aff3a9c26273f6245186ce4238016
2091a <anshulbhattglt@gmail.com>
2025-11-12 23:09:58 +0530

    multer middleware update

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,13 @@
 import multer from "multer"
+import path from "path";
+import { fileURLToPath } from "url";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp')
+    cb(null, "./public/uploads")
   },
   filename: function (req, file, cb) {
-   // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    //cb(null, file.fieldname + '-' + uniqueSuffix)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     cb(null, file.originalname)
   }
 })
